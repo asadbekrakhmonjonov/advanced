0a2492d84d27f154da618a43153efce4ed462854
Asadbek <k@Kui-MacBookAir.local>
2024-01-13 08:27:41 +0900

    added some data grouping options

---
+++ objects and methods/grouping data(dictionary).py
@@ -0,0 +1,6 @@
+name = "Atomic habits"
+year = 2018
+author = "James Clear"
+book = {"name": name, "year": year, "author": author}
+for key, value in book.items():
+    print(f"{key}: {value}")
---
+++ objects and methods/grouping data(tuples).py
@@ -0,0 +1,6 @@
+name = "Atomic habits"
+year = 2018
+author = "James Clear"
+book = (name, year, author)
+for details in book:
+    print(details)
