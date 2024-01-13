fadecf462e79e447385361340112db4e718bc1f6
Asadbek <k@Kui-MacBookAir.local>
2024-01-13 09:01:48 +0900

    changed the year

--- objects and methods/more data grouping(dictionary).py
+++ objects and methods/more data grouping(dictionary).py
@@ -1,5 +1,6 @@
 book1 = {"name": "Atomic habits","year":2018, "author": "James clear"}
 book2 = {"name": "Start with why","year": 2017,"author":"Simon Sinek"}
+book2["year"] = 2018
 for key,value in book1.items():
     print(f"{key}:{value}")
 for key,value in book2.items():
